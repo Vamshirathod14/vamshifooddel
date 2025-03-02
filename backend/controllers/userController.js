ba73f4d1115be41e74abb710011d82cad237368f
vamshi <vamshinaikramavath@gmail.com>
2025-03-02 21:19:05 +0530

    Your descriptive commit message here

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
  // controllers/userController.js
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 
 // Create a token
